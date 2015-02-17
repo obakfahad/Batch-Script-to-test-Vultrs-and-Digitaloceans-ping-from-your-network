c8f1043aa11a1e8a83fc7e0ff599673bf59442c3
Fahad <fahad@thetigerworks.com>
2015-02-17 21:49:33 +0600

    First

---
+++ Digitaloceans_Batch_Script_TO_Test_Ping.bat
@@ -0,0 +1,74 @@
+@echo off
+cd \
+echo Batch file created by Fahad Ahammed (www.obakfahad.com) >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo Digitalocean NYC1
+echo Digitalocean NYC1 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+ping 198.211.112.36
+ping 198.211.112.36 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo Digitalocean NYC2
+echo Digitalocean NYC2 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+ping 162.243.123.82
+ping 162.243.123.82 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo Digitalocean AMS1
+echo Digitalocean AMS1 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+ping 146.185.176.179
+ping 146.185.176.179 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo Digitalocean AMS2
+echo Digitalocean AMS2 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+ping 95.85.7.154
+ping 95.85.7.154 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo Digitalocean SFO1
+echo Digitalocean SFO1 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+ping 162.243.146.51
+ping 162.243.146.51 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo Digitalocean SGP1
+echo Digitalocean SGP1 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+ping 128.199.236.18
+ping 128.199.236.18 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo Digitalocean LON1
+echo Digitalocean LON1 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+ping 178.62.63.247
+ping 178.62.63.247 >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\digitalocean_ping_from_yournetwork.txt"
+pause
