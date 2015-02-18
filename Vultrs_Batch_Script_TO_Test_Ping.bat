883c5e891dd2d4695b9fee0c4379e7cdaecfb65d
Fahad <fahad@thetigerworks.com>
2015-02-18 22:59:13 +0600

    1

---
+++ Vultrs_Batch_Script_TO_Test_Ping.bat
@@ -0,0 +1,134 @@
+@echo off
+cd \
+echo Batch file created by Fahad Ahammed (www.obakfahad.com) >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ________________ASIA PACIFIC___________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo  (Asia) Tokyo, Japan
+echo  (Asia) Tokyo, Japan >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping hnd-jp-ping.vultr.com
+ping hnd-jp-ping.vultr.com >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo    (AU) Sydney, Australia
+echo    (AU) Sydney, Australia   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping   syd-au-ping.vultr.com
+ping   syd-au-ping.vultr.com >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ________________EUROPE___________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo  (EU) Frankfurt, DE
+echo  (EU) Frankfurt, DE >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping  fra-de-ping.vultr.com
+ping  fra-de-ping.vultr.com >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo   (EU) Amsterdam, NL
+echo   (EU) Amsterdam, NL >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping  ams-nl-ping.vultr.com
+ping  ams-nl-ping.vultr.com >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo   (EU) Paris, France
+echo   (EU) Paris, France  >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping  par-fr-ping.vultr.com
+ping  par-fr-ping.vultr.com >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo   (EU) London, UK
+echo   (EU) London, UK  >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping  lon-gb-ping.vultr.com
+ping  lon-gb-ping.vultr.com >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ________________NORTH AMERICA___________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo    Seattle, Washington
+echo    Seattle, Washington   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping   wa-us-ping.vultr.com
+ping   wa-us-ping.vultr.com  >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo    New York / New Jersey
+echo    New York / New Jersey   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping   nj-us-ping.vultr.com
+ping   nj-us-ping.vultr.com  >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo   Chicago, Illinois
+echo   Chicago, Illinois  >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping   il-us-ping.vultr.com
+ping   il-us-ping.vultr.com  >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo    Los Angeles, California
+echo    Los Angeles, California   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping    lax-ca-us-ping.vultr.com
+ping    lax-ca-us-ping.vultr.com   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo    Atlanta, Georgia
+echo    Atlanta, Georgia   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping    ga-us-ping.vultr.com
+ping    ga-us-ping.vultr.com   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo    Dallas, Texas
+echo    Dallas, Texas   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping   tx-us-ping.vultr.com
+ping   tx-us-ping.vultr.com >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo    Miami, Florida
+echo    Miami, Florida   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ___________________________________
+echo ___________________________________ >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+ping   fl-us-ping.vultr.com
+ping   fl-us-ping.vultr.com   >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+echo ###################################
+echo ################################### >> "C:\Users\%username%\Desktop\vultr_ping_from_yournetwork.txt"
+pause
